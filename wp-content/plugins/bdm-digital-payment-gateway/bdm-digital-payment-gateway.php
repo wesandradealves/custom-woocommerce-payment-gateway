8f7fa18a811c0b2d169a5dfd0d14b173ddd0c949
Wesley Alves <wesandradealves@gmail.com>
2025-01-27 11:29:01 -0300

    Pasta woocomerce deletada

---
+++ wp-content/plugins/bdm-digital-payment-gateway/bdm-digital-payment-gateway.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Plugin Name: WooCommerce BDM Digital Payment Gateway
+ * Plugin URI: #
+ * Description: Plugin para processamento de pagamentos em BDM Digital que utiliza endpoints para gerar QR codes, processar pagamentos, validar transações e fornecer confirmações ao usuário final. Permite integração com diferentes carteiras e serviços associados.
+ * Author: Dourado Cash
+ * Author URI: https://mercado.dourado.cash/
+ * Version: 0.0.1
+ */
