a36f8b3e56ca12a62ba488b660759921d81a31d1
Wesley Alves <wesandradealves@gmail.com>
2025-02-03 11:50:28 -0300

    feat: criar página de checkout automaticamente e associar template personalizado
    - Adiciona a criação automática da página "Checkout" ao ativar o plugin
    - Define o template `checkout-template.php` como padrão para a página
    - Registra o template no WordPress para garantir sua detecção
    - Inclui scripts e estilos específicos para a página de checkout

---
+++ bdm-digital-payment-gateway/templates/checkout-template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: BDM Checkout
+ * Description: Template for the BDM Digital Checkout page.
+ */
+
+wp_head();
+
+?>
+
+<div id="bdm-checkout-root"></div>
+
+<?php wp_footer(); ?>
