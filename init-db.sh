82838ffb086d24419be83fd1c772aab96169f308
Wesley Alves <wesandradealves@gmail.com>
2025-04-09 21:11:09 -0300

    chore(docker-flow): fix pra quebra de linhas e retorno das variaveis de ambiente corretas; corrigida instalaçao do wordpress dentro do container; corrigido fluxo de importaçao do banco;

--- init-db.sh
+++ init-db.sh
@@ -7,21 +7,17 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-# Variáveis de ambiente
-DB_HOST=${WORDPRESS_DB_HOST:-localhost}
-DB_USER=${WORDPRESS_DB_USER:-root}
-DB_PASSWORD=${WORDPRESS_DB_PASSWORD:-root}
-DB_NAME=${WORDPRESS_DB_NAME:-bdm_digital_plugin}
-
-# Verifica se o banco existe
-if ! mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -e "USE $DB_NAME;" 2>/dev/null; then
-  echo "Banco de dados '$DB_NAME' não existe. Abortando init-db."
-  exit 1
+# Verificar se o banco de dados já existe
+if ! mysql -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" -e "USE $WORDPRESS_DB_NAME"; then
+  echo "Banco de dados '$WORDPRESS_DB_NAME' não existe. Criando..."
+  mysql -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" -e "CREATE DATABASE $WORDPRESS_DB_NAME"
+else
+  echo "Banco de dados '$WORDPRESS_DB_NAME' já existe. Pulando criação."
 fi
 
 # Verifica se o banco está vazio
-TABLE_COUNT=$(mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" -e \
-  "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$DB_NAME';" -s --skip-column-names)
+TABLE_COUNT=$(mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" -e \
+  "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$WORDPRESS_DB_NAME';" -s --skip-column-names)
 
 if [ "$TABLE_COUNT" -eq 0 ]; then
   echo "O banco de dados está vazio. Verificando o arquivo SQL..."
@@ -29,7 +25,7 @@ if [ "$TABLE_COUNT" -eq 0 ]; then
 
   if [ -f "$DUMP_FILE" ]; then
     echo "Importando o arquivo SQL..."
-    if ! mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" < "$DUMP_FILE"; then
+    if ! mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" < "$DUMP_FILE"; then
       echo "Erro ao importar o dump. Verifique o conteúdo do SQL."
       exit 1
     fi
