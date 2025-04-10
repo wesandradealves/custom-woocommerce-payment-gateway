7d2d783c31638f4ee25fd233ae871a048419c925
Wesley Alves <wesandradealves@gmail.com>
2025-04-09 23:02:22 -0300

    chore(env): alteraçao da logica para alterar para o target de acordo com o env

--- init-db.sh
+++ init-db.sh
@@ -54,6 +54,7 @@ TARGET_URL="http://54.207.73.19:8000/"
 
 echo "Domínio atual: $CURRENT_URL"
 echo "Domínio de destino: $TARGET_URL"
+echo "Env: $ENVIRONMENT"
 
 if [ "$CURRENT_URL" != "$TARGET_URL" ]; then
   echo "O domínio atual ($CURRENT_URL) não corresponde ao domínio de destino ($TARGET_URL). Realizando substituição..."
@@ -63,23 +64,23 @@ if [ "$CURRENT_URL" != "$TARGET_URL" ]; then
 
   # Substituir na tabela wp_options (siteurl e home)
   mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" -e \
-    "UPDATE wp_options SET option_value = REPLACE(option_value, 'http://localhost:8000/', 'http://54.207.73.19:8000/') WHERE option_name IN ('siteurl', 'home');"
+    "UPDATE wp_options SET option_value = '$TARGET_URL' WHERE option_name IN ('siteurl', 'home');"
 
   # Substituir em wp_postmeta
   mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" -e \
-    "UPDATE wp_postmeta SET meta_value = REPLACE(meta_value, 'http://localhost:8000/', 'http://54.207.73.19:8000/') WHERE meta_value LIKE '%localhost:8000%';"
+    "UPDATE wp_postmeta SET meta_value = REPLACE(meta_value, '$CURRENT_URL', '$TARGET_URL') WHERE meta_value LIKE '%$CURRENT_URL%';"
 
   # Substituir em wp_usermeta
   mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" -e \
-    "UPDATE wp_usermeta SET meta_value = REPLACE(meta_value, 'http://localhost:8000/', 'http://54.207.73.19:8000/') WHERE meta_value LIKE '%localhost:8000%';"
+    "UPDATE wp_usermeta SET meta_value = REPLACE(meta_value, '$CURRENT_URL', '$TARGET_URL') WHERE meta_value LIKE '%$CURRENT_URL%';"
 
   # Verificar URLs no banco de dados (debugging)
   echo "Verificando URLs alteradas..."
   mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" -e \
-    "SELECT * FROM wp_postmeta WHERE meta_value LIKE '%54.207.73.19:8000%';"
+    "SELECT * FROM wp_postmeta WHERE meta_value LIKE '%$TARGET_URL%';"
 
   mysql -h "$WORDPRESS_DB_HOST" -u "$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" "$WORDPRESS_DB_NAME" -e \
-    "SELECT * FROM wp_usermeta WHERE meta_value LIKE '%54.207.73.19:8000%';"
+    "SELECT * FROM wp_usermeta WHERE meta_value LIKE '%$TARGET_URL%';"
 
   # Limpar cache do WordPress, se houver
   echo "Limpando cache do WordPress..."
